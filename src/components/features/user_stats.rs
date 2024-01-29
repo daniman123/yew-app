49b7e8782616fdda5dff77eb9fed11054b51821c
daniman123 <daniel-saleh@hotmail.com>
2024-01-29 17:09:12 +0100

    feat: implemented 'user_stats'

---
+++ src/components/features/user_stats.rs
@@ -0,0 +1,12 @@
+use yew::prelude::*;
+
+#[derive(PartialEq, Properties)]
+pub struct UserStatsProps {}
+
+#[function_component]
+pub fn UserStats(props: &UserStatsProps) -> Html {
+    let UserStatsProps {} = props;
+    html! {
+        <div></div>
+    }
+}
--- src/pages/user_stats_page.rs
+++ src/pages/user_stats_page.rs
@@ -1,47 +1,9 @@
-use web_sys::{InputEvent, MouseEvent};
 use yew::prelude::*;
-
-use crate::utils::local_storage_state_manager::{
-    event_handlers::{on_key_change, on_load, on_save, on_value_change},
-    events::{make_callback, Event},
-    local_storage_state::LocalStorageState,
-};
+use yew_app::components::features::user_stats::UserStats;
 
 #[function_component]
 pub fn UserStatsPage() -> Html {
-    let state = use_state(LocalStorageState::new);
-
-    let on_key_change_cb = {
-        let callback = make_callback(on_key_change, state.clone());
-        Callback::from(move |e: InputEvent| callback(Event::Input(e)))
-    };
-
-    let on_value_change_cb = {
-        let callback = make_callback(on_value_change, state.clone());
-        Callback::from(move |e: InputEvent| callback(Event::Input(e)))
-    };
-
-    let on_save_cb = {
-        let callback = make_callback(on_save, state.clone());
-        Callback::from(move |e: MouseEvent| callback(Event::Mouse(e)))
-    };
-
-    let on_load_cb = {
-        let callback = make_callback(on_load, state.clone());
-        Callback::from(move |e: MouseEvent| callback(Event::Mouse(e)))
-    };
-
     html! {
-        <>
-            <div>
-                <input placeholder="Key" oninput={on_key_change_cb} value={state.input_key.clone()} />
-                <input placeholder="Value" oninput={on_value_change_cb} value={state.input_value.clone()} />
-                <button onclick={on_save_cb}>{"Save"}</button>
-                <button onclick={on_load_cb}>{"Load"}</button>
-            </div>
-            <div>
-                { "Stored Value: " } { state.stored_value.clone() }
-            </div>
-        </>
+        <UserStats/>
     }
 }
